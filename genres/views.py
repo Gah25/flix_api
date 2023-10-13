f2262d34d71192e87ca58daf415c9e379776f04d
Gah25 <95923834+Gah25@users.noreply.github.com>
2023-10-13 15:49:34 -0300

    first commit flix-api

---
+++ genres/views.py
@@ -0,0 +1,13 @@
+from rest_framework import generics
+from genres.models import Genre
+from genres.serializers import GenreSerializers
+
+
+class GenreCreateListView(generics.ListCreateAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializers
+
+
+class GenreRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializers
